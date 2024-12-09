73947893817ed83d746c3cdfec5b9ad6f2ca691b
HP <mintesinotbekele1240@gmail.com>
2024-12-09 19:23:26 +0300

    Demo Server Setup!

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 const jsonServer = require('json-server');
-const path = require('path');
-
-// Create a new json-server instance
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json')); // Link to db.json
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-
-// Use default middlewares (like logging, CORS, etc.)
 server.use(middlewares);
-
-// Use the router with the database
 server.use(router);
-
-// Export the server as a serverless function
-module.exports = (req, res) => {
-  server.handle(req, res);
-};
+module.exports = server;
