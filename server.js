b8e6a6d62b6ca10d391984366216cfd802149841
HP <mintesinotbekele1240@gmail.com>
2024-12-10 16:21:40 +0300

    Demo Server Setup!

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import { connectDB } from './config/db.js'
 import userRouter from './routes/userRoute.js'
 
@@ -6,6 +7,7 @@ const app = express()
 const port = process.env.PORT || 4000;
 
 app.use(express.json())
+app.use(cors())
 
 connectDB();
 
